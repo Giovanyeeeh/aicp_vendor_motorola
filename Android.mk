vullk4n/vendor_motorola_ocean

sdm632-family: Import from 17.1
* As of 3f2c741fe258e7af051df6ed4238d073d69ad5a4.

Android.mk
@@ -1,0 +1,21 @@
+# Copyright (C) 2019-2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This file is generated by device/motorola/ocean/setup-makefiles.sh
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),ocean)
+
+endif

Giovanyeeeh/aicp_vendor_motorola

Android.mk
@@ -1,0 +1,21 @@
+# Copyright (C) 2019-2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This file is generated by device/motorola/river/setup-makefiles.sh
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),river)
+
+endif